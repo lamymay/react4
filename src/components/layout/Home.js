75d501ae085c2288af7bb7baa563478ff6a281f4
lamymay <lamymay@outlook.com>
2019-10-07 21:20:40 +0800

    菜单数据由后端获取

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import User from '../system/user/User';
 import Role from '../system/rbac/Role';
+import axios from "axios";
 
 import home from '../../assets/css/layout/home.css'
 
@@ -9,8 +10,58 @@ class Home extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            list: [
+
+
+            ]
+        }
     }
+
+    /////////////
+    //初始化函数触发
+    componentDidMount() {
+        // To disabled submit button at the beginning.
+        console.log("----------------------------");
+        console.log("componentDidMount--初始化的时候去获取菜单数据");
+        this.getMenusFun();
+        console.log("----------------------------");
+        // this.props.form.validateFields();
+    }
+
+    // 初始化的时候去获取菜单数据
+    getMenusFun = (e) => {
+        var systemId = 4;
+        var level = 0;//0 全部
+        //localhost:8001/zero/menus/2/levels/0
+        // var url = "http://arc.com/zero/menus/" + menuId + "/levels/" + level;
+        var url = "http://127.0.0.1:8001/zero/menus/" + systemId + "/levels/" + level;
+        console.log(url);
+        axios.get(url).then(response => {
+            console.log(response.data);
+
+            //失败  小于1 失败
+            if (null === response && response.data.code < 1) {
+                alert(response.data.msg);
+                console.log("FAIL");
+                this.props.history.push("/index");
+            } else {
+                //成功，获取到后台返回的数据，可以做缓存
+                console.log(" 成功" + response.data.msg);
+                // this.props.history.push("/Success");
+
+                this.setState({
+                    list: response.data.data
+                })
+            }
+        })
+            .catch(function (error) {
+                //异常
+                console.log(error);
+                console.log('异常 被 catch',);
+            });
+        ;
+    };
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////  render
@@ -19,12 +70,24 @@ class Home extends React.Component {
 
             <Router>
                 <div className="title">
-                    <Link to='/User'>User</Link>
-                    <Link to='/Role'>Role</Link>
+                    {/*<Link to='/User'>User</Link>*/}
+                    {/*<Link to='/Role'>Role</Link>*/}
+                        {
+                            this.state.list.map((value, key) => {
+                                return (
+                                    <Link key={key} to={value.name}>{value.name}</Link>
+                                )
+                            })
+                        }
+
+
                 </div>
 
-                <Route exact path="/User" component={User}/>
-                <Route exact path="/Role" component={Role}/>
+
+
+
+                <Route exact path="/user" component={User}/>
+                <Route exact path="/role" component={Role}/>
             </Router>
 
         </div>)
