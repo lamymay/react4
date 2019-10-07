5d7f230e05c03997118df7e9ce3cea35b131e9a8
lamymay <lamymay@outlook.com>
2019-10-07 22:16:11 +0800

    16

---
+++ src/components/system/project/ProjectContent.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+
+class ProjectContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            roles: [
+                {
+                    aid: '1',
+                    title: '标题1'
+                }, {
+                    aid: '2',
+                    title: '标题2'
+                }
+                , {
+                    aid: '3',
+                    title: '标题3'
+                }
+                , {
+                    aid: '4',
+                    title: '标题4'
+                }
+
+            ]
+        }
+    }
+
+
+    componentDidMount() {
+        //获取列表数据
+        // this.getProjects();
+        //赋值给 roles
+        //渲染 表格 在页面中做 新增 编辑 删除 等
+
+    }
+
+
+    getUsers = () => {
+
+    };
+
+
+    //字符串拼接
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////  render
+    render() {
+        return (
+            <div>
+                <h1>ProjectContent</h1>
+            </div>
+        )
+    }
+}
+
+export default ProjectContent;
