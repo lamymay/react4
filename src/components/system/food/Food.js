a972cb661b57716c860cf86b245078f91f4ba927
lamymay <lamymay@outlook.com>
2019-10-13 23:41:32 +0800

    添加新的模块

---
+++ src/components/system/food/Food.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+
+class Food extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                {
+                    aid: '1',
+                    title: 'food1'
+                }, {
+                    aid: '2',
+                    title: 'food2'
+                }
+                , {
+                    aid: '3',
+                    title: 'food3'
+                }
+                , {
+                    aid: '4',
+                    title: 'food4'
+                }
+
+            ]
+
+        }
+    }
+
+
+    componentDidMount() {
+
+    }
+
+
+    getFoods = () => {
+
+    };
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////  render
+    render() {
+        return (<div>
+
+            <h1>Food</h1>
+            <ul>
+                {this.state.list.map((value, key) => {
+                    return <li key={key}>
+                        <Link to={`/foodDetails?aid=${value.aid}`}>{value.title}</Link>
+                    </li>
+
+                })}
+            </ul>
+        </div>)
+    }
+}
+
+export default Food;
