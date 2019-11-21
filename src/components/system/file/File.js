685a8fdb74699966dc0fc1f0c8dcf458bcbf98d5
lamymay <lamymay@outlook.com>
2019-11-21 22:02:48 +0800

    测试react 引入图片

--- src/components/system/file/File.js
+++ src/components/system/file/File.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Upload, Icon, message, Button} from 'antd';
 import axios from "axios";
+import file from '../../../assets/css/system/file/file.css'
+
 // import img1 from "*.jpg";
 
 // const Dragger = Upload.Dragger;
@@ -13,6 +15,10 @@ class File extends React.Component {
             previewVisible: false,
             previewImage: '',
             src: '',
+            img: 'http://127.0.0.1:8001/zero/file/sunhina_157434301011046cbf990e98546bb859b5fad',
+            //T:\Project\REACT\react4\src\components\system\file
+            //T:\Project\REACT\react4\public\img
+            localImg: '%PUBLIC_URL%/image/404.jpg',
 
             fileList: [
                 {
@@ -60,7 +66,6 @@ class File extends React.Component {
     // }
 
     render() {
-
         const props = {
             name: 'file',
             // action: 'http://127.0.0.1:8001/zero/file/upload',
@@ -119,7 +124,6 @@ class File extends React.Component {
                 };*/
 
         return (
-
             <div>
 
                 <h1>欢迎|WELCOME File</h1>
@@ -152,13 +156,31 @@ class File extends React.Component {
                 <hr/>
 
                 <br/>aaa
-                <img src="http://122.51.110.127/zero/file/girl1_15725385342508ea81a509b8f4/" />
+                {/*<img src="http://122.51.110.127/zero/file/girl1_15725385342508ea81a509b8f4/" />*/}
+                {/*<img src="http://122.51.110.127/zero/file/girl1_15725385342508ea81a509b8f4/" />*/}
                 <br/>
                 <hr/>
                 {/*<img src={img1}/>*/}
 
-                {/*<img src={require('../assets/images/1.jpg')}/>*/}
 
+                <div>从后台去读去图片</div>
+
+                <img src={this.state.img} alt="" style={{
+                    width: '400px',
+                    height: '400px',
+                    textAlign: 'center'
+                }}/>
+
+
+                <div>下面是[../../../assets/image]文件中的IMG</div>
+                <img src={[require("../../../assets/image/avatar.jpeg")]} className='img-body'/>
+                {/*<img src={require('../../../../public/image/404.jpg')}/>*/}
+
+                <div>下面是[PUBLIC]文件中的IMG</div>
+                <img src={process.env.PUBLIC_URL + '/image/404.jpg'} className='img-body'/>
+
+
+                <img src="{this.img}" alt=""/>
             </div>);
     }
 
