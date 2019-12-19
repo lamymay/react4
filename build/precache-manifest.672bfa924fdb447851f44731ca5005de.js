2bc28b7dd434d881006b0deeab6fb44b2e78678f
lamymay <lamymay@outlook.com>
2019-12-19 10:26:21 +0800

    build3.1

---
+++ build/precache-manifest.672bfa924fdb447851f44731ca5005de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37c38029a3af000e431b1cfc585b238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1912f60897b78aac2629",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "64df64ce3cefa2224de8",
+    "url": "/static/css/main.f879d828.chunk.css"
+  },
+  {
+    "revision": "1912f60897b78aac2629",
+    "url": "/static/js/2.367603ee.chunk.js"
+  },
+  {
+    "revision": "64df64ce3cefa2224de8",
+    "url": "/static/js/main.3ab02a01.chunk.js"
+  },
+  {
+    "revision": "0bf5d7a0750782d31d71",
+    "url": "/static/js/runtime~main.da90fee4.js"
+  },
+  {
+    "revision": "758a4c9ebe8a8035e4a5298a302e5390",
+    "url": "/static/media/avatar.758a4c9e.jpeg"
+  }
+]);
