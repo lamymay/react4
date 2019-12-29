2d581f25951c91eda6a23f599962840681cb9c2f
lamymay <lamymay@outlook.com>
2019-12-29 19:15:35 +0800

    完善用户模块 0.1.7

---
+++ build/precache-manifest.e54e062885475b01b56dd550b3c74854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93edaf29e8be923172f441cfdc96e8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc74c5b3b76f66b78b3",
+    "url": "/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "90e478aca37d740db01e",
+    "url": "/static/css/main.79e44d3a.chunk.css"
+  },
+  {
+    "revision": "4bc74c5b3b76f66b78b3",
+    "url": "/static/js/2.54713b6a.chunk.js"
+  },
+  {
+    "revision": "90e478aca37d740db01e",
+    "url": "/static/js/main.3885d635.chunk.js"
+  },
+  {
+    "revision": "45740b11fe41e19d7e23",
+    "url": "/static/js/runtime~main.48e9854c.js"
+  },
+  {
+    "revision": "758a4c9ebe8a8035e4a5298a302e5390",
+    "url": "/static/media/avatar.758a4c9e.jpeg"
+  }
+]);
