8802757a078d12593f71ae41962b4668e69ace44
lamymay <lamymay@outlook.com>
2020-03-16 08:09:45 +0800

    npm run build 20200316

---
+++ build/precache-manifest.9f8e254a2c7e79261a229d38e05aeb26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d980846dd93ca0915c6405036cbfb6f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5088cf62f306a0a743",
+    "url": "/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "58d43c7edf57ca7b0bca",
+    "url": "/static/css/main.56c4b6e8.chunk.css"
+  },
+  {
+    "revision": "9e5088cf62f306a0a743",
+    "url": "/static/js/2.62807670.chunk.js"
+  },
+  {
+    "revision": "58d43c7edf57ca7b0bca",
+    "url": "/static/js/main.009275cb.chunk.js"
+  },
+  {
+    "revision": "45740b11fe41e19d7e23",
+    "url": "/static/js/runtime~main.48e9854c.js"
+  }
+]);
