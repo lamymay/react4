174416f470fd919a95aa7bf8e4a225e23b83afb2
yechao <wb-yc698649@alibaba-inc.com>
2020-08-05 13:11:51 +0800

    新增测试二维码的测试OK

--- src/components/test/QRTest.js
+++ src/components/test/QRTest.js
@@ -1,18 +1,16 @@
-import React from "react";
-// import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-// import user from '../../../../assets/css/system/rbac/user.css'
+//import React from "react";
+//import * as QrCode from 'qrcode.react'
+
+var React = require('react');
+var QRCode = require('qrcode.react');
 
 class QRTest extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            bgColor:'blue',
-            list: [
-
-
-            ]
-
+            data: 'http://bing.com',
+            dataTips: '请输入字符',
         }
     }
 
@@ -21,61 +19,62 @@ class QRTest extends React.Component {
 
     }
 
+    onInputChange(e) {
+        //es6变量名是一个变量
+        let inputValue = e.target.value,
+            inputName = e.target.name;
+        console.log("#####################");
+        console.log("input的名/值：" + inputName + "/" + inputValue);
+        // this.state.raw.[inputName] = inputValue
+        this.setState({
+            [inputName]: inputValue
+        })
 
-    getQRTests = () => {
+        console.log(this.state.data);
 
-    };
+        // this.setState({
+        //     name: '你猜'
+        // })
+    }
 
+    // getQRTests = () => {
+    //     var v1 = 'http://bing.com'
+    //     console.log(v1);
+    //     console.log(this.state.data);
+    // };
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////  render
+
     render() {
-        const textStyles = {
-            color: 'black',
-            backgroundColor: this.state.bgColor
-        };
+
         return (<div>
-            {/*<div  style="background-color:red;">style</div>*/}
-            <h1>QRTest</h1>
-
-            <ul>
-                {this.state.list.map((value, key) => {
-                    return <li key={key}>
-                        {/*<Link to={`/userInsert?aid=${value.aid}`}>{value.nickname}</Link>*/}
-                        <p  className='user-item'>{value.aid}|{value.nickname}</p>
-                        {/*<p  style="background-color:red;">{value.aid}|{value.nickname}</p>*/}
-                    </li>
-
-                })}
-            </ul>
             <br/>
             <br/>
-            <hr/>
-            1. 行内样式
-            <div
-                style={{
-                    width:'200px',
-                    height:'80px',
-                    backgroundColor:'yellow',
-                    fontSize:'24px',
-                    textAlign:'center'
-                }}>
-                <a href="https://github.com/lamymay/zero">后端源码 github</a>
-                <a href="https://github.com/lamymay/react4">前端源码 github</a>
+            <br/>
+
+            <div>
+                状态：<input type="text"
+                          id="data"
+                          name="data"
+                          placeholder={this.state.dataTips}
+                          onChange={e => this.onInputChange(e)}
+            />
             </div>
+            <br/>
 
-            2. 声明样式
-            <p style={textStyles}>inline style</p>
+            <div>
+                {this.state.dataTips}: {this.state.data}
+            </div>
+            <br/>
 
-        </div>)
+
+            <QRCode value={this.state.data}/>
+
+        </div>);
     }
 }
 
 export default QRTest;
 
-//https://segmentfault.com/a/1190000016952542?utm_source=tag-newest
-//测试引入css的方式
-// 1、行内引入， 特点：即在行内直接写，未能测试通过，报错，切支持度按网上说法也不是很全要驼峰编写
-// 2、页内引入，特点：写const变量，这样数据与js会在一起，维护调试不是很方便
-//3、外部引入，推荐，特点：与原生几乎一样，
-// 第三种的用法：1js头部 “import user from '../../../../assets/css/system/rbac/user.css'” 2使用 "<p  className='user-item'>{value.aid}|{value.nickname}</p>"
+
